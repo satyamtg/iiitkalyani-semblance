526ac1e845a13a69a1f2f73a4e706d8a28a3d063
JokerPI <ankur.bidhannagar@gmail.com>
2018-07-02 22:42:05 +0530

    Events page without data  and navbar completed

--- css/semblance.js
+++ css/semblance.js
@@ -1,4 +1,5 @@
-var txt = "events";
+var txt = document.getElementById("eventshead").innerHTML;
+var txt2 = document.getElementById("detail").innerHTML;
 console.log(txt);
 var j=0; var k=0;
 var speed = 250;
@@ -11,7 +12,18 @@ function typeWriter() {
   }
 }
 
+
+function typeWriter2() {
+  if (k < txt2.length) {
+    document.getElementById("detail").innerHTML += txt2.charAt(k);
+    k++;
+    setTimeout(typeWriter2, 30);
+  }
+}
+
 window.onload = function(){
   document.getElementById("eventshead").innerHTML = " ";
   typeWriter();
+  document.getElementById("detail").innerHTML = " ";
+  setTimeout(typeWriter2, 6000);
 };
